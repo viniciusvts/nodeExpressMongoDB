66eaa50e945aee26f8ff693bf822506213e02c3c
Vinicius de Santana <vinicius.vts@gmail.com>
2019-04-11 01:17:12 -0300

    codigo nao usado

--- assets/keys.js
+++
@@ -1,8 +0,0 @@
-const keys = {
-    mongodb: 'mongodb://localhost:27017/test',
-    //mongodb: 'mongodb://localhost:27017/test' //para testes
-    //mongodb: 'mongodb://mongodb:27017/test' //para o docker
-    
-    jsonWebToken: 'senhasegura(ounao)'
-}
-module.exports = keys;
