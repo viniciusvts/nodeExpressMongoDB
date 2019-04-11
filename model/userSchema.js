a844c01cf1d0a280accb9f4704b5cfd7a8281293
Vinicius de Santana <vinicius.vts@gmail.com>
2019-04-11 01:17:35 -0300

    criptografia para salvar senhas

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
 
 const userSchema = new Schema({
     email: { type: String, required: true, unique: true, lowercase: true },
     pass: { type:String, required: true, select:false },
     create: { type: Date, default: Date.now }
 });
+
+/**
+ * @description encriptar a senha quando salvar
+ * afinal ninguem quer suas senhas aparecendo
+ */
+userSchema.pre('save', function(next){
+    let user = this;
+    if ( !user.isModified('pass') ) return next;
+    bcrypt.hash( user.pass, 10, (err, encrypted)=>{
+        user.pass = encrypted;
+        return next();
+    });
+});
+
 module.exports = mongoose.model( 'User', userSchema);
