c441c1be0f457363fa873cbbaf0bd89a5acd17db
Vinicius de Santana <vinicius.vts@gmail.com>
2019-04-13 18:18:26 -0300

    agora verifico se o usuário é admin

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,26 +2,37 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 const jwtKey = config.jsonWebToken;
 const jwtExpiresTime = config.jsonWebTokenExpiresTime;
+const Users = require('../model/userSchema');
 
 const auth = {
     
     /**
-     * verifica o token enviado pelo usuário
+     * @description verifica se o token enviado pelo usuário
+     * corresponde a um usuário autenticado, para seer usado como middleware
      */
     verify: function(req, res, next){
         console.log("APP: auth.verify");
         const tokenHeader = req.headers.token;
+
         if(!tokenHeader) return res.status(401).send({ error: "Token ausente"});
+
         jwt.verify( tokenHeader, jwtKey, (err, decoded)=>{
             if(err) return res.status(401).send({ error: "Token incorreto"});
+
             res.locals.token_id = decoded;
-            return next();
+            Users.findOne( {_id: res.locals.token_id.id }, (err, data)=>{
+                if (err) return res.status(500).send( {error: "Erro no banco"});
+                if (data){
+                    res.locals.isAdmin = data.admin;
+                    return next();
+                }
+            })
         });
     },
 
     /**
-     * @argument userId recebe objeto no formato: { id: dataId }
-    * @description cria um token para o usuário
+    * @argument userId recebe objeto no formato: { id: dataId }
+    * @description cria um token para o usuário autenticado
     */
     create: function(userId){
         console.log("APP: auth.create");
